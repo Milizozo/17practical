a3082e3c9ab004c7abd9e0ae7c04f5fd7d6cf7fa
Milizozo <4454895@myuwc.ac.za>
2026-03-25 14:27:31 +0200

    inserting numbers in the range

--- tryBST.java
+++ tryBST.java
@@ -1,2 +1,20 @@
 //comment 1
 //comment 2
+import java.util.*;
+class tNode{
+    int key;
+    tNode left, right;
+
+    public tNode(int key){
+        this.key = key;
+        left = null;
+        right = null;
+    }
+}
+
+class BST{
+    tNode root;
+    BST() {
+        root = null;
+    }
+  //comment 3
